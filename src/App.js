6b5b93a8a8a86a4f2a6097974605e3be522c75a3
hamish taylor <hamish@raygun.com>
2023-08-24 15:41:59 +1200

    Added some comments

--- src/App.js
+++ src/App.js
@@ -25,10 +25,11 @@ var schema = buildSchema(`
 // The root provides a resolver function for each API endpoint
 var root = {
     quoteOfTheDay: function(args) {
-        if (args.num > 1) {
+        if (args.num > 1) { //Handle incorrect inputs
+            //Create a new error with a desciptive message
             return new GraphQLError('num must be less than 1', {
-                extensions: {
-                    code: "FORBIDDEN",
+                extensions: { //Can add custom data to the error
+                    code: "FORBIDDEN", //Custom Error codes can be used to identify the error type
                     SomeOtherKey: "Some other value",
                 },
             })
@@ -59,16 +60,17 @@ function App() {
 
     var processesGraphQLError = function(request) {
         if (request.errors) {
-            request.errors.forEach(function(err) {
+            request.errors.forEach(function(err) { //Loop through each of the reported errors
                 console.log(err);
                 rg4js('send', {
                     error: err,
-                    customData: [{ key: 'query', value: err.source }],
-                    tags: [err.extensions.code],
+                    customData: [{ key: 'query', value: err.source }], //Send the query that caused the issue as custom data
+                    tags: [err.extensions.code], //Use the error code as a tag, this could also be prepended to the name of the error.
                 }
                 );
             })
         }
+        //Deal with the non error data.
     };
 
     return (
